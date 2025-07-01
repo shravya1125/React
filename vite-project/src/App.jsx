ea28cb449ea5ca90c68fceb1388796cb857a9ce8
shravya1125 <rsati0125@gmail.com>
2025-07-01 19:05:12 +0530

    vite-react  project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1> hello namaste react</h1>
+  )
+}
+
+export default App
