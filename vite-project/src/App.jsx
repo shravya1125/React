5f2ea5e29ee549581bdea157ec565240df546aad
shravya1125 <rsati0125@gmail.com>
2025-07-02 21:52:49 +0530

    comp

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
-
+import Sim from './sim.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1> hello namaste react</h1>
+    //<h1> hello namaste react</h1>
+    <>
+    <Sim/>
+    <h1> learning react</h1>
+    <p>this is paragraph</p>
+    </>
+    
+
   )
 }
 
---
+++ vite-project/src/sim.jsx
@@ -0,0 +1,7 @@
+function Sim(){
+    return(
+        <h3>simple page is ready</h3>
+    )
+}
+
+export default Sim;
